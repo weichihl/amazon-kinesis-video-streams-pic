d65e46709d5cca5f12cc78e62f4883885efd12e3
HungTseLee <hungtselee@realtek.com>
2021-01-19 10:17:16 +0800

    update atomics for IAR and gnu

--- src/utils/src/AtomicsGnu.h
+++ src/utils/src/AtomicsGnu.h
@@ -6,7 +6,6 @@ extern "C" {
 #endif
 
 #include "Include_i.h"
-#include <stdatomic.h>
   
 #ifdef __clang__
 #    pragma clang diagnostic push
@@ -16,71 +15,124 @@ extern "C" {
 #    pragma GCC diagnostic ignored "-Wpedantic"
 #endif
 
+#if defined (__ICCARM__)
+#include <stdatomic.h>  
+
 static inline SIZE_T defaultAtomicLoad(volatile SIZE_T* pAtomic)
 {
-    //return __atomic_load_n(pAtomic, __ATOMIC_SEQ_CST);
     return atomic_load((atomic_uint*)pAtomic);
 }
 
 static inline VOID defaultAtomicStore(volatile SIZE_T* pAtomic, SIZE_T var)
 {
-    //__atomic_store_n(pAtomic, var, __ATOMIC_SEQ_CST);
     atomic_store((atomic_uint*)pAtomic, var);
 }
 
 static inline SIZE_T defaultAtomicExchange(volatile SIZE_T* pAtomic, SIZE_T var)
 {
-    //return __atomic_exchange_n(pAtomic, var, __ATOMIC_SEQ_CST);
     return atomic_exchange((atomic_uint*)pAtomic, var);
 }
 
 static inline BOOL defaultAtomicCompareExchange(volatile SIZE_T* pAtomic, SIZE_T* pExpected, SIZE_T desired)
 {
-    //return __atomic_compare_exchange_n(pAtomic, pExpected, desired, FALSE, __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST);
     return atomic_compare_exchange_strong((atomic_uint*)pAtomic, pExpected, desired);
 }
 
 static inline SIZE_T defaultAtomicIncrement(volatile SIZE_T* pAtomic)
 {
-    //return __atomic_fetch_add(pAtomic, 1, __ATOMIC_SEQ_CST);
     return atomic_fetch_add((atomic_uint*)pAtomic, 1);
 }
 
 static inline SIZE_T defaultAtomicDecrement(volatile SIZE_T* pAtomic)
 {
-    //return __atomic_fetch_sub(pAtomic, 1, __ATOMIC_SEQ_CST);
     return atomic_fetch_sub((atomic_uint*)pAtomic, 1);
 }
 
 static inline SIZE_T defaultAtomicAdd(volatile SIZE_T* pAtomic, SIZE_T var)
 {
-    //return __atomic_fetch_add(pAtomic, var, __ATOMIC_SEQ_CST);
     return atomic_fetch_add((atomic_uint*)pAtomic, var);
 }
 
 static inline SIZE_T defaultAtomicSubtract(volatile SIZE_T* pAtomic, SIZE_T var)
 {
-    //return __atomic_fetch_sub(pAtomic, var, __ATOMIC_SEQ_CST);
     return atomic_fetch_sub((atomic_uint*)pAtomic, var);
 }
 
 static inline SIZE_T defaultAtomicAnd(volatile SIZE_T* pAtomic, SIZE_T var)
 {
-    //return __atomic_fetch_and(pAtomic, var, __ATOMIC_SEQ_CST);
     return atomic_fetch_and((atomic_uint*)pAtomic, var);
 }
 
 static inline SIZE_T defaultAtomicOr(volatile SIZE_T* pAtomic, SIZE_T var)
 {
-    //return __atomic_fetch_or(pAtomic, var, __ATOMIC_SEQ_CST);
     return atomic_fetch_or((atomic_uint*)pAtomic, var);
 }
 
 static inline SIZE_T defaultAtomicXor(volatile SIZE_T* pAtomic, SIZE_T var)
 {
-    //return __atomic_fetch_xor(pAtomic, var, __ATOMIC_SEQ_CST);
     return atomic_fetch_xor((atomic_uint*)pAtomic, var);
 }
+
+#else  //for GCC toolchain
+static inline SIZE_T defaultAtomicLoad(volatile SIZE_T* pAtomic)
+{
+    return __atomic_load_n(pAtomic, __ATOMIC_SEQ_CST);
+}
+
+static inline VOID defaultAtomicStore(volatile SIZE_T* pAtomic, SIZE_T var)
+{
+    __atomic_store_n(pAtomic, var, __ATOMIC_SEQ_CST);
+}
+
+static inline SIZE_T defaultAtomicExchange(volatile SIZE_T* pAtomic, SIZE_T var)
+{
+    return __atomic_exchange_n(pAtomic, var, __ATOMIC_SEQ_CST);
+}
+
+static inline BOOL defaultAtomicCompareExchange(volatile SIZE_T* pAtomic, SIZE_T* pExpected, SIZE_T desired)
+{
+    return __atomic_compare_exchange_n(pAtomic, pExpected, desired, FALSE, __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST);
+}
+
+static inline SIZE_T defaultAtomicIncrement(volatile SIZE_T* pAtomic)
+{
+    return __atomic_fetch_add(pAtomic, 1, __ATOMIC_SEQ_CST);
+}
+
+static inline SIZE_T defaultAtomicDecrement(volatile SIZE_T* pAtomic)
+{
+    return __atomic_fetch_sub(pAtomic, 1, __ATOMIC_SEQ_CST);
+}
+
+static inline SIZE_T defaultAtomicAdd(volatile SIZE_T* pAtomic, SIZE_T var)
+{
+    return __atomic_fetch_add(pAtomic, var, __ATOMIC_SEQ_CST);
+}
+
+static inline SIZE_T defaultAtomicSubtract(volatile SIZE_T* pAtomic, SIZE_T var)
+{
+    return __atomic_fetch_sub(pAtomic, var, __ATOMIC_SEQ_CST);
+}
+
+static inline SIZE_T defaultAtomicAnd(volatile SIZE_T* pAtomic, SIZE_T var)
+{
+    return __atomic_fetch_and(pAtomic, var, __ATOMIC_SEQ_CST);
+}
+
+static inline SIZE_T defaultAtomicOr(volatile SIZE_T* pAtomic, SIZE_T var)
+{
+    return __atomic_fetch_or(pAtomic, var, __ATOMIC_SEQ_CST);
+}
+
+static inline SIZE_T defaultAtomicXor(volatile SIZE_T* pAtomic, SIZE_T var)
+{
+    return __atomic_fetch_xor(pAtomic, var, __ATOMIC_SEQ_CST);
+}
+
+#endif
+
+
+
 
 #ifdef  __cplusplus
 }
